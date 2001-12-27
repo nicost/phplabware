d7f4c1f0bb5f046ab3bc102c5b1248794ba58912
nicost <>
2001-12-27 00:57:27 +0000

    Added PWD_MINIMUM

--- phplabware/includes/defines_inc.php
+++ phplabware/includes/defines_inc.php
@@ -3,16 +3,18 @@
 // defines_inc.php - constants used in phplabware
 // defines_inc.php - author: Nico Stuurman
 
-
 // defines permissions
 $ACTIVE=1;
 $READ=2;
 $WRITE=4;
 $EDIT=8;
 $LAYOUT=16;
+$ADMIN=32;
 $SUPER=64;
 
 // the following will be needed in most scripts
 $PHP_SELF=$HTTP_SERVER_VARS["PHP_SELF"];
 
+// minimum password length
+$PWD_MINIMUM=4;
 ?>
