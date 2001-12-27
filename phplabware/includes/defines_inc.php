0e783a2b400cc8507803862ea4a30fc3e09e14bf
nicost <>
2001-12-27 05:57:46 +0000

    Changed header and added variable tables

--- phplabware/includes/defines_inc.php
+++ phplabware/includes/defines_inc.php
@@ -2,6 +2,16 @@
 
 // defines_inc.php - constants used in phplabware
 // defines_inc.php - author: Nico Stuurman
+  /***************************************************************************
+  * Copyright (c) 2001 by Nico Stuurman                                      *
+  * ------------------------------------------------------------------------ *
+  *  Part of phplabware, a web-driven groupware suite for research labs      *
+  *                                                                          *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/  
 
 // defines permissions
 $ACTIVE=1;
@@ -12,9 +22,13 @@ $LAYOUT=16;
 $ADMIN=32;
 $SUPER=64;
 
-// the following will be needed in most scripts
+// the following is needed in most scripts
 $PHP_SELF=$HTTP_SERVER_VARS["PHP_SELF"];
 
 // minimum password length
 $PWD_MINIMUM=4;
+
+// comma-separated list (no spaces!) of tables containing user entries
+// these tables should have the field 'userid'
+$tables="";
 ?>
