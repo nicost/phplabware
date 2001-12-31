2a25d1f425caf96128823fabc6affd01c64b7e79
nicost <>
2001-12-31 00:49:19 +0000

    Changed call to navbar

--- phplabware/logout.php
+++ phplabware/logout.php
@@ -6,7 +6,7 @@
 session_start();
 include ('includes/functions_inc.php');
 printheader("Loging out");
-navbar($USER["permissions"]);
+navbar(false);
 if ($HTTP_SESSION_VARS["PHP_AUTH_USER"]) {
    session_destroy();
    echo "&nbsp;<br><h3 align='center'>Thanks for logging out!</hr><br>&nbsp;";
