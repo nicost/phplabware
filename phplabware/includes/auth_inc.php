0062ed6b348133a34de83b792d58c8b3e59cb24a
nicost <>
2001-12-31 00:52:59 +0000

    bugfixes and URL change to allow for cookie-less sessions

--- phplabware/includes/auth_inc.php
+++ phplabware/includes/auth_inc.php
@@ -14,122 +14,11 @@
   *  option) any later version.                                              *
   \**************************************************************************/
 
-////
-// !class to get and store browser/OS info
-class cl_client {
-   var $browser;
-   var $OS;
 
-   function cl_client () {
-      $HTTP_USER_AGENT = getenv (HTTP_USER_AGENT);
-      $temp = strtolower ($HTTP_USER_AGENT);
-      if (strstr($temp, "opera"))
-         $this->browser = "Opera";
-      elseif (strstr($temp, "msie"))
-         $this->browser = "Internet Explorer";
-      else
-         $this->browser = "Netscape or the like";
-      if (strstr($temp, "windows"))
-         $this->OS = "Windows";
-      elseif (strstr($temp, "linux"))
-         $this->OS = "Linux";
-      elseif (strstr($temp, "sunos"))
-         $this->OS = "Sun";
-      elseif (strstr($temp, "mac"))
-         $this->OS = "Mac OS";
-      elseif (strstr($temp, "irix"))
-         $this->OS = "IRIX";
-   }
-}      
+//$post_vars="logon,user,pwd,ssl,submit";
+//globalize_vars($post_vars,"HTTP_POST_VARS");
+//echo "$user,$pwd,$ssl,$logon,$ssl,$post_vars.<br>";
 
-////
-// !function that checks if user is allowed to view page
-// This function should be called before a printheader() call
-function allowonly($required, $current) {
-   if (! ($required & $current)) {
-      printheader("Not Allowed");
-      navbar(1);
-      echo "&nbsp;<br><h3 align='center'>Sorry, but this page is not for you!";
-      echo "</hr><br>&nbsp;\n";
-      printfooter();
-      exit;
-   }
-}
-
-
-////
-// returns a link to a randomly selected image from the designated directory
-// imagedir should be relative to the running script and be web-accessible
-function randomImage($imagedir) {
-   // determine contents of imagedir and store imagefilenames in imagearray
-   $dirhandle = opendir ("$imagedir");
-   if (!$dirhandle)
-      return false;
-   $j = 0;
-   while ($file = readdir ($dirhandle)) {
-      if (strstr ($file, ".png") || strstr($file, ".jpg") || 
-                                    strstr($file,".gif") ) {
-         $imagearray[$j]=$file;
-         $j++;
-      }
-   }
-   $filecount=sizeof ($imagearray);
-   // no files
-   if (!$filecount)
-      return false;
-   // 'select' a random file
-   srand((double)microtime()*1000000);
-   if ($filecount>1)
-      $filenr=rand (0,$filecount-1);
-   else
-      $filenr=0;
-   // construct link to randomly selected file
-   $filename=$imagearray[$filenr];
-   return "<img src='$imagedir/$filename'>\n";   
-}
-
-
-////
-// !presents the login screen when authenticating witth sessions
-function loginscreen ($message="<h3>Login to PhpLabWare</h3>") {
-   global $HTTP_SERVER_VARS, $settings;
-
-   $PHP_SELF=$HTTP_SERVER_VARS["PHP_SELF"];
-   if ($settings["secure_server"]) {
-      $server= getenv ("HTTP_HOST");
-      $addres="https://$server$PHP_SELF";
-   }
-   else
-      $addres=$PHP_SELF;
-
-   printheader ("Login to PhpLabWare");
-   echo "<form name='loginform' method='post' action=$addres>\n";
-   echo "<input type='hidden' name='logon' value='true'>\n";
-   echo "<table align=center>\n";
-   echo "<tr><td colspan=2 align='center'>$message</td>\n";
-   $imstring = randomimage("frontims");
-   if ($imstring);
-      echo "<td rowspan=6>&nbsp;&nbsp&nbsp;$imstring</td>";
-   echo "</tr>\n";
-   echo "<tr><td>Your login name:</td>\n";
-   echo "<td><input name='user' size=10 value=''></td></tr>\n";
-   echo "<tr><td>Password:</td>\n";
-   echo "<td><input type='password' name='pwd' size=10 value=''></td></tr>\n";
-   echo "<tr><td colspan=2 align='center'>";
-   if ($settings["secure_server"]) {
-      echo "<input type='checkbox' name='ssl'>Keep a secure connection";
-   }
-   echo "</td></tr>\n";
-   echo "<tr><td colspan=2 align='center'>";
-   echo "<input type='submit' name='submit' value='Login'></td></tr>\n";
-   echo "<tr><td colspan=2 align='center'>";
-   echo "Note:  Cookies must be enabled beyond this point</td></tr>\n";
-   echo "</table>\n";
-   printfooter();
-   exit();
-}
-
-/******************** start of global script ********************/
 
 $client = new cl_client;
 
@@ -143,7 +32,7 @@ if ($use_sessions) {
    session_start();
 
    // if this is a login, authenticate the user:
-   if ($HTTP_POST_VARS["logon"]) {
+   if ($HTTP_POST_VARS["logon"]=="true") {
       $PHP_AUTH_USER=$HTTP_POST_VARS["user"];
       $PHP_AUTH_PW=$HTTP_POST_VARS["pwd"];
       if ($PHP_AUTH_USER && $PHP_AUTH_PW) {
@@ -153,7 +42,7 @@ if ($use_sessions) {
          $db_query = "SELECT login FROM users WHERE login='$PHP_AUTH_USER' AND pwd='$pwd'";
          $db_result = $db->Execute($db_query);
          if ($db_result)
-            $auth=$db_result->fields[0];
+            $auth=$db_result->fields["login"];
          // check that there is no one else like this
          $db_result->Movenext();
          if (!$db_result->EOF)
@@ -176,6 +65,7 @@ if ($use_sessions) {
  
          // if authenticated, this session is OK:
          if ($auth) {
+            $HTTP_SESSION_VARS["PHP_AUTH_USER"]=$PHP_AUTH_USER;
             session_register ("PHP_AUTH_USER");
             // when the login was secure but user does not wanna stay secure
             if (getenv("HTTPS") && !$HTTP_POST_VARS["ssl"]) {
@@ -184,7 +74,10 @@ if ($use_sessions) {
                $server= getenv ("HTTP_HOST");
                $url="http://$server$PHP_SELF";
                echo "<html>\n<head>\n";
-               echo "<meta http-equiv='refresh' content=0;URL='$url'>\n";
+?>
+<meta http-equiv='refresh' content=0;URL="<?php echo $url ?>?<?=SID?>">
+<?php
+               //echo "<meta http-equiv='refresh' content=0;URL='$url'>\n";
                echo "</head>\n</html>";
                exit();
             } 
@@ -195,8 +88,10 @@ if ($use_sessions) {
             exit();
          }
       }
-      else 
+      else { 
          loginscreen("<h4>Please enter your username and password</h4>");
+         exit();
+      }
    } 
 
    // if the $PHP_AUTH_USER is not set, we need to identify and authenticate 
@@ -205,6 +100,7 @@ if ($use_sessions) {
    if (!$PHP_AUTH_USER) {
       // display logon screen
       loginscreen();
+      exit();
    } 
    else {
       // we must have been authenticated directly or through the session
