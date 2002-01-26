da0224817f91301ab07b6453d0cd53e60963fc3e
nicost <>
2002-01-26 07:12:59 +0000

    Changed type of field files/type to text, removed table filetypes

--- phplabware/dd/0_0022_inc.php
+++ phplabware/dd/0_0022_inc.php
@@ -57,26 +57,7 @@ $tablesid=$db->GenID("tableoftables_id_seq");
 $query="INSERT INTO tableoftables VALUES ($tablesid,200,'protocols','pr')";
 if (!$db->Execute($query)) $test=false;
 
-$query="ALTER TABLE files ADD COLUMN type int";
-if (!$db->Execute($query)) $test=false;
-
-$query="CREATE TABLE filetypes
-        (id int PRIMARY KEY,
-	 sortkey int,
-	 type text,
-	 typeshort text)";
-if (!$db->Execute($query)) $test=false;
-$db->Execute("CREATE INDEX filetypes_id_index ON filetypes (id)");
-$db->Execute("CREATE INDEX filetypes_sortkey_index ON filetypes (sortkey)");
-
-$fid=$db->GenID("filetypes_id_seq");
-$query="INSERT INTO filetypes VALUES ($fid,100,'HTML','html')";
-if (!$db->Execute($query)) $test=false;
-$fid=$db->GenID("filetypes_id_seq");
-$query="INSERT INTO filetypes VALUES ($fid,200,'MSWord','msword')";
-if (!$db->Execute($query)) $test=false;
-$fid=$db->GenID("filetypes_id_seq");
-$query="INSERT INTO filetypes VALUES ($fid,300,'PDF','pdf')";
+$query="ALTER TABLE files ADD COLUMN type text";
 if (!$db->Execute($query)) $test=false;
 
 ?>
