4c77073e818148f9afd4c5f9e362ac8f8af72b66
nicost <>
2002-01-30 20:20:38 +0000

    Now redirects to a system-set homeURL after logging out

--- phplabware/logout.php
+++ phplabware/logout.php
@@ -8,13 +8,20 @@ include ("includes/init_inc.php");
 if (isset($system_settings["tmpdir"]))
    session_save_path($system_settings["tmpdir"]);
 session_start();
-printheader("Loging out");
-navbar(false);
+if (isset($system_settings["homeURL"])) {
+   $head="<META HTTP-EQUIV='Refresh' CONTENT='1;URL=".
+          $system_settings["homeURL"]."'>\n";
+}
 if ($HTTP_SESSION_VARS["PHP_AUTH_USER"]) {
+   printheader("Loging out",$head);
+   navbar(false);
    session_destroy();
    echo "&nbsp;<br><h3 align='center'>Thanks for logging out!</h5><br>&nbsp;";
 }
-else
+else {
+   printheader("Loging out");
+   navbar(false);
    echo "&nbsp;<br><h3 align='center'>You better login first!</hr><br>&nbsp;";
+}
 printfooter();
 ?>
