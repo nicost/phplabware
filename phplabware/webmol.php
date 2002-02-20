2f5b9b51938a1e9c9f6919c9d2fb0e0f0f9fe90d
nicost <>
2002-02-20 01:24:18 +0000

    made it look nicer

--- phplabware/webmol.php
+++ phplabware/webmol.php
@@ -6,6 +6,7 @@
 // This code is Free Software and licensed under the GPL version 2 or later (your choice).  Do some cool stuff with it!
 
 
+require ("include.php");
 // pdbid can be transfered as a GET or POST variable
 $pdbid=$HTTP_GET_VARS["pdbid"];
 if (!$pdbid) 
@@ -14,16 +15,22 @@ if (!$pdbid) {
    echo "It does not make much sense to launch webmol without a molecule...";
    exit();
 }
+$pdbid=strtolower($pdbid);
 // set wherever you'll run Webmol from:
 $codebase="http://www.cmpharm.ucsf.edu/~walther/webmol/";
 
+
+$httptitle .= "Webmol:$pdbid";
+printheader ($httptitle);
+navbar($USER["permissions"]);
 ?>
-<html><body>
+<table align='center' border='0'>
+<tr><td>
 <applet code="proteinViewer.class"
 codebase=<?php echo "'$codebase'"; ?>
-  width=700 height=600>
+  width=700 height=600 align='center'>
 <PARAM NAME="PROTEIN"
-VALUE="1crn">
+VALUE=<?php echo "'$pdbid'";?>>
 <PARAM NAME="PDB_STRING"
 VALUE="">
 <PARAM NAME="PATH" VALUE="">
@@ -32,4 +39,8 @@ VALUE="">
 <PARAM NAME="SearchString"
 VALUE="varSnfnvcrl|C|lpgtPEaicaty">
 </applet>
-</body> </html>
+</tr></td>
+</table>
+<?php
+printfooter ();
+?>
