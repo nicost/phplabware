807bc049d9c0d5ff72dbd6ec971a77917cc972a3
nicost <>
2002-04-17 04:49:58 +0000

    New table trust

---
+++ phplabware/dd/0_0030_inc.php
@@ -0,0 +1,25 @@
+<?php
+
+// 0_0030_inc.php - Adds table trust to refine access rights
+// 0_0030_inc.php - author: Nico Stuurman
+
+  /***************************************************************************
+  * Copyright (c) 2002 by Nico Stuurman                                      *
+  * ------------------------------------------------------------------------ *
+  * This code is part of phplabware (http://phplabware.sf.net)               *
+  *                                                                          *
+  *                                                                          *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/ 
+
+
+$db->Execute("CREATE TABLE trust (
+              tableid int,
+              recorid int,
+              trusteduserid int)");
+
+
+?>
