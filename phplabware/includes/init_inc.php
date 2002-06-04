b822211fc8517b9ec92b12e0dace4e42e375632c
nicost <>
2002-06-04 05:02:10 +0000

    Removed persistent connection to allow working with temporary tables

--- phplabware/includes/init_inc.php
+++ phplabware/includes/init_inc.php
@@ -33,7 +33,7 @@ $ADODB_FETCH_MODE=ADODB_FETCH_DEFAULT;
 
 // Open connection to the database
 $db = NewADOConnection ($db_type);
-if (!@$db->PConnect($db_host, $db_user, $db_pwd, $db_name)) {
+if (!@$db->Connect($db_host, $db_user, $db_pwd, $db_name)) {
    echo "<h3 color='red'>Fatal Error</h3>";
    echo "Could not connect to the database server.<br>";
    echo "Please report this problem to your system administrator.";
