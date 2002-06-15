05573ecf5790c6bf0d49fde143a0d080c0f66033
nicost <>
2002-06-15 06:47:36 +0000

    now also generates new windows

--- phplabware/includes/js/linkmenu.js
+++ phplabware/includes/js/linkmenu.js
@@ -1,7 +1,13 @@
 function linkmenu (selectmenu) {
 	selecteditem=selectmenu.selectedIndex;
 	newurl=selectmenu.options[selecteditem].value;
-	if (newurl.length!=0) {
-		location.href=newurl;
+	if (newurl.indexOf("target ")==0) {
+		newurl=newurl.slice(7);
+		var newWindow=open(newurl,"otherWindow","scrollbars=1,resizable=1,statusbarbar=1,navigator=1,defaultStatus=1,width=800,height=400,top=1");
+	}
+	else {
+		if (newurl.length!=0) {
+			location.href=newurl;
+		}
 	}
 }
