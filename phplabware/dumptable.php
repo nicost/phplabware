f2aff951c75e6d6ad6f56d9b93f8b4b7d2060cae
nicost <>
2002-07-21 22:49:25 +0000

    Looks pretty good by now.  Only thing missing is relational aspect (links to other tables)

--- phplabware/dumptable.php
+++ phplabware/dumptable.php
@@ -31,21 +31,35 @@ $outfile=$system_settings["tmpdir"]."/dumptable.php";
 $fp=fopen($outfile,"w");
 if (!$fp) {
    echo "<h3 align='center'>Failed to open <i>$outfile</i> for output</h3>\n";
-   printfooter($d, $USER);
+   printfooter($db, $USER);
 }
 
 $header="<?php\n\n\n\n\n";
 fwrite($fp,$header);
 
-fwrite ($fp,'$newtableid=$db->GenID("tableoftables_id_seq");'."\n");
+fwrite ($fp,'$newtableid=$db->GenID("tableoftables_gen_id_seq");'."\n");
 fwrite ($fp,'$newtablename='."$tablename;\n");
+fwrite ($fp,'$newtablelabel='."\"$table_label\";\n");
+fwrite ($fp,'for ($i=0;$i<$hownew;$i++) {$newtablelabel.="new";}'."\n");
 fwrite ($fp, 'while (get_cell($db,"tableoftables","id","tablename",$newtablename)) {
-   $newtablename.="-new";
+   $newtablename.="new";
+   $hownew++;
 }
-$newtableshortname=substr($newtablename,0,3);
+for ($i=0;$i<$hownew;$i++) 
+   $newtablelabel.="new";
+$newtableshortname=substr($newtablename,0,3).$newtableid;
 $newtable_realname=$newtablename."_".$newtableid;
 $newtable_desc_name=$newtable_realname."_desc";
-$r=$db->Execute("INSERT INTO tableoftables id,sortkey,tablename,shortname,display,permission,custom,real_tablename,table_desc_name,label VALUES (\'$newtableid\',\'0\',\'$newtablename\',\'$newtableshortname\',\'Y\',\'Users\',NULL,\'$newtable_realname\',\'$newtable_desc_name\','."'$table_label')\");\n");
+$r=$db->Execute("INSERT INTO tableoftables (id,sortkey,tablename,shortname,display,permission,custom,real_tablename,table_desc_name,label) VALUES (\'$newtableid\',\'0\',\'$newtablename\',\'$newtableshortname\',\'Y\',\'Users\',NULL,\'$newtable_realname\',\'$newtable_desc_name\',\'$newtablelabel\')");'."\n");
+
+fwrite ($fp, '$rg=$db->Execute ("SELECT id FROM groups");
+while (!$rg->EOF) {
+   $groupid=$rg->fields[0];
+   $db->Execute ("INSERT INTO groupxtable_display VALUES ($groupid,$newtableid)");
+   $rg->MoveNext();
+}
+
+');
 
 fwrite ($fp, 'if ($r) {
    $rb=$db->Execute("CREATE TABLE $newtable_desc_name (
@@ -60,6 +74,7 @@ fwrite ($fp, 'if ($r) {
       datatype text,
       associated_table text,
       associated_column text,
+      associated_local_key text,
       thumb_x_size int,
       thumb_y_size int,
       link_first text,
@@ -67,7 +82,7 @@ fwrite ($fp, 'if ($r) {
       modifiable char(1) )");
    if ($rb) {
       ');
-$desc_fields="sortkey,label,columnname,display_table,display_record,required,type,datatype,associated_table,associated_column,thumb_x_size,thumb_y_size,link_first,link_last,modifiable";
+$desc_fields="sortkey,label,columnname,display_table,display_record,required,type,datatype,associated_table,associated_column,associated_local_key,thumb_x_size,thumb_y_size,link_first,link_last,modifiable";
 $ADODB_FETCH_MODE=ADODB_FETCH_NUM;
 $s=$db->Execute("SELECT $desc_fields FROM $table_desc");
 while (!$s->EOF) {
@@ -85,6 +100,24 @@ while (!$s->EOF) {
    }
    fwrite ($fp,')");
       ');
+   // recreate pull down tables
+   if ($s->fields[7]=="pulldown") {
+      fwrite($fp,'$ass_table=$newtable_realname."ass";
+      $id_ass=$db->GenId($ass_table,20);
+      $ass_table.="_$id_ass";
+      $db->Execute("CREATE TABLE $ass_table (
+         id int PRIMARY KEY,
+         sortkey int,
+         type text,
+         typeshort text)");
+      $db->Execute("UPDATE $newtable_desc_name SET associated_table=\'$ass_table\' WHERE id=$newid");
+      ');
+   }
+   // destroy links to tables, since those will fail
+   elseif ($s->fields[7]=="table") {
+      fwrite ($fp,'$db->Execute("UPDATE $newtable_desc_name SET associated_table=NULL,associated_column=NULL WHERE id=$newid");
+      ');
+   }
    $s->MoveNext();
 }
 
