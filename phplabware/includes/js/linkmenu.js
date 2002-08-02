6e58347bb529b84c58ac06e10bde96011cb627ff
nicost <>
2002-08-02 16:29:54 +0000

    Newly opend windows now have a toolbar

--- phplabware/includes/js/linkmenu.js
+++ phplabware/includes/js/linkmenu.js
@@ -3,7 +3,7 @@ function linkmenu (selectmenu) {
 	newurl=selectmenu.options[selecteditem].value;
 	if (newurl.indexOf("target ")==0) {
 		newurl=newurl.slice(7);
-		var newWindow=open(newurl,"otherWindow","scrollbars=1,resizable=1,statusbarbar=1,navigator=1,defaultStatus=1,width=800,height=400,top=1");
+		var newWindow=open(newurl,"otherWindow","scrollbars=1,resizable=1,statusbarbar=1,toolbar=1,navigator=1,defaultStatus=1,width=800,height=400,top=1");
 	}
 	else {
 		if (newurl.length!=0) {
