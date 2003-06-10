0b038698de95dc9b0ea6dfebb782347ae725e8fa
nicost <>
2003-06-10 00:43:56 +0000

    Move contents fo column access to gr,gw,er.ew

---
+++ phplabware/dd/0_2002_inc.php
@@ -0,0 +1,34 @@
+<?php
+
+// Translates columns access into columns gr,gw,er,ew
+
+unset ($rb);
+$rb=$db->Execute("SELECT real_tablename FROM tableoftables");
+while (!($rb->EOF)) {
+   if($rb->fields['real_tablename']) {
+echo "{$rb->fields['real_tablename']}...<br>";
+      unset ($rc);
+      $rc=$db->Execute("SELECT id,access FROM {$rb->fields['real_tablename']}");
+      while ($rc && !$rc->EOF) {
+         if ($rc->fields['access']{3}=='r')
+            $gr=1;
+         else
+            $gr=0;
+         if ($rc->fields['access']{4}=='w')
+            $gw=1;
+         else
+            $gw=0;
+         if ($rc->fields['access']{6}=='r')
+            $er=1;
+         else
+            $er=0;
+         if ($rc->fields['access']{7}=='w')
+            $ew=1;
+         else
+            $ew=0;
+         $db->Execute("UPDATE {$rb->fields['real_tablename']} SET gr=$gr,gw=$gw,er=$er,ew=$ew WHERE id={$rc->fields['id']}");
+         $rc->MoveNext();
+      }
+   }
+   $rb->MoveNext();
+}
