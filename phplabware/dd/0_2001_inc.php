6c9b667c65d20851edf855482588c09ba4a9da5c
nicost <>
2003-06-11 04:49:30 +0000

    Takes care that sequences to desc tables exist and are in sync with reality

--- phplabware/dd/0_2001_inc.php
+++ phplabware/dd/0_2001_inc.php
@@ -2,7 +2,6 @@
 
 // Adds columns gw, gr, ow, or to each table
 // Adodb datadictionary is used to add these plus indices
-$db->debug=true;
 
 $ra=$db->Execute("SELECT real_tablename,table_desc_name,shortname FROM tableoftables");
 $dict=NewDataDictionary($db);
@@ -14,10 +13,12 @@ $fields="
 ";
 $fieldstring="id,label,columnname,sortkey,display_table,display_record, required, type, datatype, associated_table, associated_column, modifiable"; 
 while (!($ra->EOF)) {
-   if($ra->fields['real_tablename']) {
+   if($ra->fields['table_desc_name']) {
       $shortname=$ra->fields['shortname']; 
+      // add the new columns
       $sqlArray=$dict->AddColumnSQL($ra->fields['real_tablename'],$fields);
       $dict->ExecuteSQLArray($sqlArray);
+      // and make indices on them
       $sqlArray=$dict->CreateIndexSQL($shortname.'_gr_index',$ra->fields['real_tablename'],'gr');
       $dict->ExecuteSQLArray($sqlArray);
       $sqlArray=$dict->CreateIndexSQL($shortname.'_gw_index',$ra->fields['real_tablename'],'gw');
@@ -29,7 +30,15 @@ while (!($ra->EOF)) {
 
       // update description tables
       $desc=$ra->fields['table_desc_name'];
-      $descid=$db->GenId("$desc"."_id");  
+      // check that $desc_id (sequence) is in sync with the table, and get it in sync if it was not:
+      $rt=$db->Execute("SELECT max(id) FROM $desc");
+      $max=$rt->fields[0];
+      if ($max) {
+         while ($descid <=$max)
+            $descid=$db->GenId("$desc"."_id");  
+      }
+      else
+         $descid=$db->GenId("$desc"."_id");  
       $db->Execute("INSERT INTO $desc ($fieldstring) Values($descid,'group read','gr','111','N','N','N','smallint','int',NULL,NULL,'Y')");
       $descid=$db->GenId("$desc"."_id");  
       $db->Execute("INSERT INTO $desc ($fieldstring) Values($descid,'group write','gw','112','N','N','N','smallint','int',NULL,NULL,'Y')");
