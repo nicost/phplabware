b50ca6307631898ba1ae2c132826cbaf0df1a950
nicost <>
2004-09-12 19:29:01 +0000

    function now works as standalone, test more, and then generalize to loop through all columns of type file of a phplabware installation

--- phplabware/dd/0_4200_inc.php
+++ phplabware/dd/0_4200_inc.php
@@ -1,17 +1,28 @@
 <?php
+include ('./include.php');
 // For the sake of full text searches, from now on only store the first page on which a word occurred.  Here, we delete all entries with later occurrences:
 
-function clean_text_column($table)
+function clean_text_column($db, $table)
 {
    $r=$db->Execute("SELECT wordid,fileid,pagenr,recordid FROM $table ORDER BY pagenr");
-   while (!$r->eof()) {
+   while (!$r->EOF) {
+      $result['totalcounter']+=1;
       $rdouble=$db->Execute("SELECT wordid,fileid,pagenr,recordid FROM $table WHERE wordid={$r->fields[0]} AND fileid={$r->fields[1]} AND recordid={$r->fields[3]} ORDER BY pagenr");
        $rdouble->MoveNext();
        if ($rdouble->Numrows() > 1) {
-          while (!$rdouble->eof()) {
+          while (!$rdouble->EOF) {
              $db->Execute("DELETE FROM $table WHERE wordid={$rdouble->fields[0]} AND fileid={$rdouble->fields[1]} AND pagenr={$rdouble->fields[2]} AND recordid={$rdouble->fields[3]}");
+             $result['delcounter']+=1;
              $rdouble->MoveNext();
           }
        }
+      $r->MoveNext();
    }
+   return ($result);
 }
+
+$table='files_10001_wi_9';
+$result=clean_text_column($db, $table);
+if (!isset($result['delcounter'])) $result['delcounter']=0;
+echo "Deleted {$result['delcounter']} records outof a total of {$result['totalcounter']} from table: $table.<br>\n";
+?>
