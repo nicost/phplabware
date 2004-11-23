6d49d1c1a0dc41029d92194417326b64abffcf43
nicost <>
2004-11-23 06:12:05 +0000

    Added some protection against abuse and a string to date converter

--- phplabware/actionMode.php
+++ phplabware/actionMode.php
@@ -19,9 +19,18 @@
 require ('./include.php');
 require('./includes/db_inc.php');
 
-
 $tableinfo=new tableinfo($db,false,$_POST['tableid']);
 
+// don't have these fields changes:
+$forbidden_fields=array ('id','ownerid');
+if (in_array($_POST['field'],$forbidden_fields)) {
+   return false;
+}
+if ($_POST['datatype']=='date') {
+   $_POST['newvalue']=strtotime($_POST_newvalue);
+}
+
+//$db->debug=true;
 if (may_see_table($db,$USER,$tableinfo->id) && may_write($db,$tableinfo->id,$_post['recordid'],$USER)) {
    $db->Execute("UPDATE {$tableinfo->realname} SET {$_POST['field']}='{$_POST['newvalue']}' WHERE id={$_POST['recordid']}");
 }
