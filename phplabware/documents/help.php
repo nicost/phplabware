f5f683be4eb625a716ae876bd7a685f42993873f
nicost <>
2005-01-24 01:06:22 +0000

    Added explanation of URL-based logins

--- phplabware/documents/help.php
+++ phplabware/documents/help.php
@@ -4,7 +4,7 @@
 <li>A header section that will be displayed only once.  The end of the header section is indicated by: <br><pre>
 &lt;!--fields--&gt;
 </pre></li>
-<li>A section that will be displayed for every record in the current search result set.  The text will displayed as is, except that the SQL columnnames of the table, preceded with a '$' sign will be replaced with the text as displayed by phplabware for that record and column.  The SQL columnname preceded with a '&' sign will be replaced by the actual content of that record and column in the database.  As an example:<br><pre>
+<li>A section that will be displayed for every record in the current search result set.  The text will displayed as is, except that the SQL columnnames of the table, preceded with a '$' sign will be replaced with the text as displayed by phplabware for that record and column.  The SQL columnname preceded with a '%' sign will be replaced by the actual content of that record and column in the database.  As an example:<br><pre>
 <b>$counter. </b>$title,$image,$title,$link,$tests,$file<br />
 </pre></li>
 <li>A footer section that will be displayed once at the bottom of the report. It should be preceded with:<br><pre>
@@ -14,3 +14,9 @@ In the footer section, every SQL columnname preceded with the '&' sign will be r
 </li>
 </ul>
 <p>Both the header and footer sections are optional.  As an example, look at the file reports.txt.</p>
+
+
+<h3>URLs linking into phplabware</h3>
+<p>As of version 0.5 of phplabware, login information can be provided in the URL itself, allowing direct linking to any recod within phplabware.  For this to work, you need to set this option in 'System' -> 'settings' (Allow logins that have, etc..) and you will need to check the box next to 'Allow URL-based login' in the user's settings.  Now, a URL of the form:</p>
+<p>http://mysite/phplabware/login.php?user=guest&pwd=guest&javascript_enabled=true</p>
+<p>will automate the login procedure.  It is adviceable to set the last option (javascript_enable=true) using javascript.</p>
