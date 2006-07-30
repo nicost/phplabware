eff79d85c295560a997f6dca6d7f86fd17444805
nicost <>
2006-07-30 00:31:01 +0000

    Version 0.61

--- phplabware/setup.php
+++ phplabware/setup.php
@@ -12,7 +12,7 @@
   *  option) any later version.                                              *
   \**************************************************************************/                                                                             
 
-$version_code=0.6;
+$version_code=0.61;
 $adodb_version_required=3.50;
 $localdir=exec('pwd');
 include ('./includes/functions_inc.php');
