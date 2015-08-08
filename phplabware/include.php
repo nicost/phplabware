33640888087ea4f6a679bbbe6e1e48de20b531a3
Nico Stuurman <nico.stuurman@ucsf.edu>
2015-08-07 22:10:23 -0700

    application wide avoidance of causing cross-site scripting possiblities using the script url

--- phplabware/include.php
+++ phplabware/include.php
@@ -1,5 +1,6 @@
 <?php
-
+// avoid cross-site scripting issues in one central location
+$_SERVER['PHP_SELF'] = htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES);
 include ('./includes/defines_inc.php');
 include ('./includes/functions_inc.php');
 include ('./includes/init_inc.php');
